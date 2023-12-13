eb77f5016d2d0dd7790edcc062ef20dd2687aa0b
Nicholas <54176339+Nicholas-cool@users.noreply.github.com>
2023-12-13 07:57:06 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,12 +1,12 @@
 function skillsMember() {
-    var member = document.getElementById("member");
-    var memberSkills = document.getElementById("memberSkills");
-    var memberSkills2 = document.getElementById("memberSkills2");
-    var memberSkills3 = document.getElementById("memberSkills3");
-    var memberSkills4 = document.getElementById("memberSkills4");
-    var memberSkills5 = document.getElementById("memberSkills5");
-    var memberSkills6 = document.getElementById("memberSkills6");
-    var memberSkills7 = document.getElementById("memberSkills7");
-    var memberSkills8 = document.getElementById("memberSkills8");
-    var memberSkills9 = document.getElementById("memberSkills9");
+    return {
+        restrict: 'E',
+        templateUrl: 'templates/skills/view/member.html',
+        controller: 'SkillsMemberController',
+        controllerAs: 'vm',
+        bindToController: true,
+        scope: {
+            member: '='
+        }
+    };
 }
